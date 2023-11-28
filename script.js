8fda0e6b2b0684e261bbc05b653ce1166cd7315c
Kat Pokorny <145707652+katpoko@users.noreply.github.com>
2023-11-28 20:27:25 +0000

    add java script

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
 console.log( '😀👍🍎' );
+// GSAP library
+// https://unpkg.co/gsap@3/dist/gsap.min.js
+
+// ScrollTrigger plugin
+// https://unpkg.com/gsap@3/dist/ScrollTrigger.min.js
+
+// Install Plugin
+gsap.registerPlugin(ScrollTrigger);
+
+// Get each Panel
+let sections = gsap.utils.toArray(".panel");
+// Set Panels to horizontally scroll
+gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".container",
+    pin: true,
+    scrub: 1,
+    snap: 1 / (sections.length - 1),
+    // base vertical scrolling on how wide the container is so it feels more natural.
+    end: "+=3500"
+  }
+});
+
+// Panel One
+gsap.to(".one", {
+  scrollTrigger: "one"
+});
+
+// Panel Two
+gsap.to(".two", {
+  scrollTrigger: "two"
+});
+
+// Panel Three
+gsap.to(".three", {
+  scrollTrigger: "three"
+});
+
+// Panel Four
+gsap.to(".four", {
+  scrollTrigger: "four",
+  color: "red"
+});
+
+// Panel Five
+gsap.to(".five h1", {
+  scrollTrigger: "five"
+});
